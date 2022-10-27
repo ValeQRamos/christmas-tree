ec698e8b38767e9229ba27342d919bdc7d06b331
vale quiroga <valentinqramos@outlook.com>
2022-10-27 14:06:31 -0500

    first and final commit

---
+++ scritp.js
@@ -0,0 +1,20 @@
+
+  console.log(" ".repeat(8) + "*");
+    for(var  i = 0 ; i < 9 ; i++){
+    console.log(" ".repeat(8-i) + "0".repeat(2*i+1));
+    }
+
+
+  // for (let i = 1; i <= 9; i++) {
+  //   str = "";
+  //   for (let j = i; j < 9; j++) {
+  //     str += " ";
+  //   }
+  //   for (let k = 0; k < i * 2 - 1; k++) {
+  //     str += "0";
+  //   }
+  //   if (i === 1) {
+  //     console.log("        *");
+  //   }
+  //   console.log(str);
+  // }
